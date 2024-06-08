7d5e7e06731e32dd703f32e03dbd11764fcb7b88
blissful <blissful@sunsetglow.net>
2024-06-08 15:46:58 -0400

    add simple bash build system

--- build.sh
+++ build.sh
@@ -9,4 +9,13 @@ mkdir dist
 # Directly copy over all assets.
 cp -r src/assets dist/assets
 # Directly copy over all HTML files.
-find src/ -type f -name "*.html" | xargs -I{} sh -c 'cp "{}" "$(echo "{}" | sed "s/^src/dist/")"'
+find src/ -type f -name "*.html" -print0 | while IFS= read -r -d '' file; do
+    cp "$file" "${file/#src/dist}"
+done
+# Compile LaTex blog posts.
+find src/ -type f -name "*.tex" -print0 | while IFS= read -r -d '' file; do
+    out="$(echo "$file" | perl -pe 's/^src\/(.*)\.tex/dist\/\1.html/')"
+    mkdir --parents "$(dirname "$out")"
+    pandoc --standalone "$file" --table-of-contents -o "$out"
+    perl -0777 -i -pe 's/^.*?<body>\s*(.*?)\s*<\/body>\s*<\/html>\s*$/\1/s' "$out"
+done
---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "sunsetglow";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive) scheme-small
+            amsmath
+            dirtree
+            ellipsis
+            enumitem
+            etoc
+            etoolbox
+            fontspec
+            footmisc
+            setspace
+            titlesec
+            ;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            (pkgs.buildEnv {
+              name = "studies-env";
+              paths = with pkgs; [
+                coreutils
+                moreutils
+                findutils
+                tex
+                pandoc
+              ];
+            })
+          ];
+          shellHook = ''
+            find-up () {
+              path=$(pwd)
+              while [[ "$path" != "" && ! -e "$path/$1" ]]; do
+                path=''${path%/*}
+              done
+              echo "$path"
+            }
+
+            export PROJECT_ROOT="$(find-up flake.nix)"
+          '';
+        };
+      }
+    );
+}
